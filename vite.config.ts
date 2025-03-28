78c2ec17f2caf92afca03227097f3d221b27f032
Dev Marangoni <joao.tadeuzi00@gmail.com>
2025-03-27 23:29:25 -0300

    config vercel

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,20 @@
-import path from "path"
-import tailwindcss from "@tailwindcss/vite"
-import react from "@vitejs/plugin-react"
-import { defineConfig } from "vite"
+import path from "path";
+import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
 
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [react()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
   },
-})
+  build: {
+    outDir: "dist"
+  },
+  preview: {
+    headers: {
+      "Cache-Control": "no-store"
+    },
+  },
+});
